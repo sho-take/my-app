7cf92fab90a2a72e1de567d168ce9bfdecb9e2a7
sho-take <takesyou12@gmail.com>
2025-01-31 14:50:18 +0900

    Update progress.tsx

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -17,10 +17,15 @@ const Progress = React.forwardRef<
     )}
     {...props}
   >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
+ <ProgressPrimitive.Indicator
+  className="h-full w-full flex-1 transition-all"
+  style={{
+    backgroundColor: "blue",
+    opacity: 0.3, // 透明度を設定（0.1 ～ 1）
+    transform: `translateX(-${100 - (value || 0)}%)`
+  }}
+/>
+
   </ProgressPrimitive.Root>
 ))
 Progress.displayName = ProgressPrimitive.Root.displayName
